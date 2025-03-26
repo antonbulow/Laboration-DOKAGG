3596a2018228a17e16b6c14b2583a66c809e35cc
antonbulow <antonbul@chalmers.se>
2025-03-26 16:20:59 +0100

    start 1

--- lib/pages/main_view.dart
+++ lib/pages/main_view.dart
@@ -1,19 +1,30 @@
 import 'package:flutter/material.dart';
 
+
 class MainView extends StatelessWidget {
   const MainView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(
+        title: const Text('Att Göra'),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      ),
+
+      // body: Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: [
+      //       Text('DAT216/TIG091'),
+      //       Text('Lab1 Todos'),
+      //     ],
+        body: ListView(
           children: [
-            Text('DAT216/TIG091'),
-            Text('Lab1 Todos'),
-          ],
-        ),
+            ListTile(title: Text('DAT216/TIG091')),
+            ListTile(title: Text('Lab1 Todos')),
+            ListTile(title: Text('En rad till')),
+        ],
       ),
     );
   }
